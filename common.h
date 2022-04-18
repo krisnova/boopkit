f4625eba8c1c4ec2d01f1df52270e3296f943659
Kris Nóva <kris@nivenly.com>
2022-04-18 09:21:01 -0400

    Automatic commit from github.com/kris-nova/bin/git-save
    Signed-off-by: Kris Nóva <kris@nivenly.com>

--- common.h
+++ common.h
@@ -5,6 +5,9 @@
 #ifndef BOOPKIT_COMMON_H
 #define BOOPKIT_COMMON_H
 
+
+#include <stddef.h>
+
 #define MAX_BOOP_PRINTF_LOG 1024
 #define BOOPKIT_RCE_DELIMITER "*~*"
 
@@ -13,5 +16,14 @@ extern int quiet;
 
 void asciiheader();
 void boopprintf(const char *__restrict __format, ...);
+char *base64_encode(const unsigned char *data,
+                    size_t input_length,
+                    size_t *output_length);
+unsigned char *base64_decode(const unsigned char *data,
+                             size_t input_length,
+                             size_t *output_length);
+
+void build_decoding_table();
+void base64_cleanup();
 
 #endif  // BOOPKIT_COMMON_H
