389819fb7d60c67d38b550370d753f0c0e384ab2
Kris Nóva <kris@nivenly.com>
2022-04-19 17:42:26 -0400

    Automatic commit from github.com/kris-nova/bin/git-save
    Signed-off-by: Kris Nóva <kris@nivenly.com>

--- common.h
+++ common.h
@@ -11,7 +11,7 @@
 extern int quiet;
 
 // VERSION is the semantic version of the program
-#define VERSION "1.3.0"
+#define VERSION "1.4.0"
 
 void asciiheader();
 void boopprintf(const char *__restrict __format, ...);
